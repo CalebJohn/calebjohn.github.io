363a582fca2a7299432b999d8a3eae634f96c6c9
Caleb John <calebjohn4@gmail.com>
2013-05-22 19:51:13 -0500

    Update popup.js

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,6 +1,5 @@
 function popup()
   {
     alert("Alert:\n\nUploading Virus.png to host webservers\n\n\nPlease Confirm Upload:");
-    for (i=0;i<600000000;i++){}
     alert("\n\n\nUpload Successful\n\n");
   }
