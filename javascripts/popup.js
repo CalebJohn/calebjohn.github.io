e5584e2ab0afd85a1fd1a3394a0b117c0b0b70af
Caleb John <calebjohn4@gmail.com>
2013-05-24 20:44:43 -0600

    adds more popups

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,5 +1,29 @@
-function popup()
+started = false;
+
+function parent()
   {
     alert("Alert:\n\nUploading Virus.png to host webservers\n\n\nPlease Confirm Upload:");
     alert("\n\n\nUpload Successful\n\n");
+    started = true;
+  }
+
+function child()
+  {
+    phrases = ['Hello! My name is Popup but you can call me junior..', '..You may have met my father already..', '..I\'m sorry if he was a little rude, ever since he started drinking..', '..Nevemind, I\'ll make sure he doesn\'t bug you anymore'];
+    for (i in phrases)
+    {
+      alert(phrases[i]);
+    }
+  }
+
+function popup()
+  {
+    if (started)
+    {
+    child();
+    }
+    else
+    {
+    parent();
+    }
   }
