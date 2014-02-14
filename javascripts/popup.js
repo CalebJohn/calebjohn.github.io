60168e737b7464c6e94bdac6495583c1eff60e99
Caleb John <calebjohn4@gmail.com>
2014-02-14 13:44:11 -0700

    removes popup child and adds virus.png

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,42 +1,26 @@
 started = false;
-childRun = false;
 
 function parent()
-  {
-    alert("Alert:\n\nUploading Virus.png to host webservers\n\n\nPlease Confirm Upload:");
-    alert("\n\n\nUpload Successful\n\n");
-    
-    started = true;
-  }
-
-function child()
-  {
-    phrases = ['Hello! My name is Popup but you can call me junior..', '..You may have met my father already..', '..I\'m sorry if he was a little rude, ever since he started drinking..', '..Nevemind, I\'ll make sure he doesn\'t bug you anymore'];
-    for (i in phrases)
-    {
-      alert(phrases[i]);
-    }
-    
-    childRun = true;
-  }
+{
+  alert("Alert:\n\nUploading Virus.png to host webservers\n\n\nPlease Confirm Upload:");
+  alert("\n\n\nUpload Successful\n\n");
+  
+  started = true;
+}
 
 function popup()
+{
+  if (started)
   {
-    if (childRun)
+    console.log(document.URL.substr(-10));
+    // Must navigate to the index page first
+    if (document.URL.substr(-10) == "index.html")
     {
-      console.log(document.URL.substr(-10));
-      // Must navigate to the index page first
-      if (document.URL.substr(-10) == "index.html")
-      {
-        window.location.replace("Riddler/riddler.html");
-      }
-    }
-    else if (started)
-    {
-      child();
-    }
-    else
-    {
-      parent();
+      window.location.replace("Riddler/riddler.html");
     }
   }
+  else
+  {
+    parent();
+  }
+}
