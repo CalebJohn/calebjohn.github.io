a03586bfcb32e88a812b9a1ad627ec1b99faf019
Caleb John <calebjohn4@gmail.com>
2014-02-20 18:49:43 -0700

    Add virus download

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,8 +1,31 @@
 started = false;
 
-function parent()
+function virus()
 {
   alert("Alert:\n\nUploading Virus.png to host webservers\n\n\nPlease Confirm Upload:");
+  
+  // Taken from an external source
+  //////////////////////////////////
+  var downloadLink = document.createElement("a");
+  downloadLink.download = "virus.png";
+  var filenameBlob = new Blob(["http://www.calebjohn.ca/javascripts/virus.png"], {type:'text/plain'})
+  if (window.webkitURL != null)
+  {
+      // Chrome allows the link to be clicked
+      // without actually adding it to the DOM.
+      downloadLink.href = window.webkitURL.createObjectURL(filenameBlob);
+  }
+  else
+  {
+      // Firefox requires the link to be added to the DOM
+      // before it can be clicked.
+      downloadLink.href = window.URL.createObjectURL(filenameBlob);
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+  }
+  downloadLink.click();
+  //////////////////////////////////
+  
   alert("\n\n\nUpload Successful\n\n");
   
   started = true;
@@ -21,6 +44,6 @@ function popup()
   }
   else
   {
-    parent();
+    virus();
   }
 }
